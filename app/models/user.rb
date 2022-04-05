9d74ec2424a5dd668b62e88feb4505967e44d4ed
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-05 20:54:15 +0900

    コードレビューを元に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname,         presence: true
   validates :last_name,        presence: true,
-                               format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :first_name,       presence: true,
-                               format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
   validates :last_name_kana,   presence: true,
                                format: { with: /\A[ァ-ヶー－]+\z/,
                                          message: 'is invalid. Input full-width katakana characters' }
