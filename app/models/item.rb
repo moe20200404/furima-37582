15c44e2faafee807766f3214e59cee722cbf5996
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-10 08:56:15 +0900

    categoryとstatusのアクティブハッシュ実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,29 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+
   validates :name,                    presence: true
+
   validates :info,                    presence: true
-  validates :category_id,             presence: true       
-  validates :status_id,               presence: true     
-  validates :shopping_fee_status_id,  presence: true                  
-  validates :prefecture_id,           presence: true         
-  validates :scheduled_delivery_id,   presence: true                 
+
+  validates :category_id,             presence: true,
+                                      numericality: { other_than: 1 , message: "can't be blank"}       
+  
+  validates :status_id,               presence: true,
+                                      numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :shopping_fee_status_id,  presence: true,
+                                      numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :prefecture_id,           presence: true,
+                                      numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :scheduled_delivery_id,   presence: true,
+                                      numericality: { other_than: 1 , message: "can't be blank"}
+                                                       
   validates :price,                   presence: true 
   validates :user,                    presence: true
 end
