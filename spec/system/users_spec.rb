510083104722bc86d99a62b57379674c81b7e890
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-10 21:30:53 +0900

    Itemの結合テストコード

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -117,8 +117,4 @@ RSpec.describe 'ユーザーログアウト', type: :system do
   end
 end
 
-def basic_pass(path)
-  username = ENV['BASIC_AUTH_USER']
-  password = ENV['BASIC_AUTH_PASSWORD']
-  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
-end
+
