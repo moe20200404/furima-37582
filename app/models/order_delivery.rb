93adf590fc7cdbf57e76df319aafae3910a7c6bd
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-17 15:18:27 +0900

    手順1の実行

---
+++ app/models/order_delivery.rb
@@ -0,0 +1,23 @@
+class OrderDelivery
+  include ActiveModel::Model
+    attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building ,:phone_number ,:order
+
+  validates :postal_code,    presence: true
+  validates :postal_code,    format: { with: /\A\d{3}-\d{4}\z/ }, allow_blank: true
+
+  validates :prefecture_id,  presence: true
+  validates :prefecture_id,  numericality: { other_than: 1, message: "can't be blank" }, allow_blank: true
+
+  validates :city,           presence: true
+  validates :addresses,      presence: true
+  validates :building,       
+  validates :phone_number,   presence: true
+  validates :phone_number,   format: { with: /\A\d{11}\z/ }, allow_blank: true
+
+  validates :order,          presence: true
+
+  def save
+    order = Order.create(user_id: current_user.id, item_id: order.item_id)
+    Delivery.createcreate(postal_code: postal_code, prefecture_id: prefecture_id, city: city, adrreses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
