c0a6a90d937e2391614f296a9257e78741731eab
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-17 17:55:52 +0900

    手順4の実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,17 @@ class OrdersController < ApplicationController
   end
   def create
     @order_delivery = OrderDelivery.new(params_order)
-    if @order_delivery.save
+    if @order_delivery.valid?
+      @order_delivery.save
       redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
 private
   def params_order
-    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building ,:phone_number ,:order
+    attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building ,:phone_number ,:user_id, :item_id
 
   validates :postal_code,    presence: true
   validates :postal_code,    format: { with: /\A\d{3}-\d{4}\z/ }, allow_blank: true
@@ -14,10 +14,9 @@ class OrderDelivery
   validates :phone_number,   presence: true
   validates :phone_number,   format: { with: /\A\d{11}\z/ }, allow_blank: true
 
-  validates :order,          presence: true
-
   def save
+    binding.pry
     order = Order.create(user_id: user_id, item_id: item_id)
-    Delivery.createcreate(postal_code: postal_code, prefecture_id: prefecture_id, city: city, adrreses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+    Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
