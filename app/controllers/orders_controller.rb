bc18df64d4dc5d2000e0b9049b1b308d81b0aabf
moe20200404 <98138987+moe20200404@users.noreply.github.com>
2022-04-24 16:52:36 +0900

    STEP2手順2~4の実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,9 @@ class OrdersController < ApplicationController
   end
   def create
     @order_delivery = OrderDelivery.new(params_order)
+    @item = Item.find(params[:item_id])
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
@@ -16,6 +18,15 @@ class OrdersController < ApplicationController
 
 private
   def params_order
-    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params_order[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building ,:phone_number ,:user_id, :item_id
+    attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building ,:phone_number ,:user_id, :item_id, :token
 
   validates :postal_code,    presence: true
   validates :postal_code,    format: { with: /\A\d{3}-\d{4}\z/, message: "input correctly"}
@@ -10,13 +10,12 @@ class OrderDelivery
 
   validates :city,           presence: true
   validates :addresses,      presence: true
-  # validates :building,       
   validates :phone_number,   presence: true
   validates :phone_number,   format: { with: /\A\d{10,11}\z/, message: "input only number"}
+  validates :token,          presence: true
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
-    binding.pry
   end
 end
